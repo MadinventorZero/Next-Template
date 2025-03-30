e40ae0e90c6129f45e25af4dfc9b2d6ca304e96e
MadinventorZero <42157117+MadinventorZero@users.noreply.github.com>
2025-03-30 08:55:14 +0000

    wip: add react query, jest config, prettier

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+ 
+const createJestConfig = nextJest({
+  dir: './',
+})
+ 
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+  }
+}
+ 
+export default createJestConfig(config)
---
+++ prettier.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+};
+
+module.exports = config;
